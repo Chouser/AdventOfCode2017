5652f65fe0af9546a4c3a7e21750d25bf18dbd5a
Chouser <chouser@n01se.net>
2018-01-07 21:36:52 -0500

    day18: add runtime tracing to both impls

--- src/chouser/day18.clj
+++ src/chouser/day18.clj
@@ -4,25 +4,25 @@
 (def input
   (vec
    (for [line (line-seq (io/reader "resources/input18.txt"))]
-     (re-seq #"\S+" line))))
+     (read-string (str \[ line \])))))
 
 (defn step [{:keys [prog played ip recover] :as s}]
   (let [[op x y] (get prog ip)
-        v (fn [string]
-            (if (re-matches #"[a-z]" string)
-              (get s string 0)
-              (Long/parseLong string)))
+        v (fn [x]
+            (if (symbol? x)
+              (get s x 0)
+              x))
         s (update s :ip inc)]
     (case op
-      "snd" (assoc s :played (v x))
-      "set" (assoc s x (v y))
-      "add" (update s x (fnil + 0) (v y))
-      "mul" (update s x (fnil * 0) (v y))
-      "mod" (update s x (fnil rem 0)(v y))
-      "rcv" (if (zero? (v x)) s (recover s))
-      "jgz" (if (pos? (v x))
-              (update s :ip + (dec (v y)))
-              s))))
+      snd (assoc s :played (v x))
+      set (assoc s x (v y))
+      add (update s x (fnil + 0) (v y))
+      mul (update s x (fnil * 0) (v y))
+      mod (update s x (fnil rem 0)(v y))
+      rcv (if (zero? (v x)) s (recover s))
+      jgz (if (pos? (v x))
+            (update s :ip + (dec (v y)))
+            s))))
 
 ;; (prn (dissoc (step {:prog input :ip 0}) :prog))
 
@@ -33,39 +33,41 @@
 
 ;; part 2
 
-(defn step [{:keys [prog played ip inbox] :as s}]
-  (let [[op x y] (get prog ip)
-        ;;_ (prn op x y (-> s (dissoc :prog) (update :inbox seq)))
-        v (fn [string]
-            (if (re-matches #"[a-z]" string)
-              (get s string 0)
-              (Long/parseLong string)))
+(defn step [{:keys [prog ip inbox] :as s}]
+  (let [[op x y :as cmd] (nth prog ip)
+        v (fn [x]
+            (if (symbol? x)
+              (get s x 0)
+              x))
         s (-> s
+              (update :trace conj {:ip ip :cmd cmd
+                                   :state (into {} (filter #(symbol? (key %)) s))})
               (update :ip inc)
               (assoc :blocked false))]
     (case op
-      "snd" (-> s
-                (update :outbox conj (v x))
-                (update :send-count inc))
-      "set" (assoc s x (v y))
-      "add" (update s x (fnil + 0) (v y))
-      "mul" (update s x (fnil * 0) (v y))
-      "mod" (update s x (fnil rem 0)(v y))
-      "rcv" (if (seq inbox)
-              (-> s
-                  (assoc x (peek inbox))
-                  (update :inbox pop))
-              (-> s
-                  (assoc :blocked true)
-                  (update :ip dec)))
-      "jgz" (if (pos? (v x))
-              (update s :ip + (dec (v y)))
-              s))))
+      snd (-> s
+              (update :outbox conj (v x))
+              (update :send-count inc))
+      set (assoc s x (v y))
+      add (update s x (fnil +   0) (v y))
+      mul (update s x (fnil *   0) (v y))
+      mod (update s x (fnil rem 0) (v y))
+      rcv (if (seq inbox)
+            (-> s
+                (assoc x (peek inbox))
+                (update :inbox pop))
+            (-> s
+                (assoc :blocked true)
+                (update :ip dec)))
+      jgz (if (pos? (v x))
+            (update s :ip + (dec (v y)))
+            s))))
 
 (def init {:ip 0
            :send-count 0
            :outbox []
-           :inbox clojure.lang.PersistentQueue/EMPTY})
+           :inbox clojure.lang.PersistentQueue/EMPTY
+           :trace []})
 
 (defn move-msgs [from to ss]
   (-> ss
@@ -73,11 +75,12 @@
       (update-in [from :outbox] empty)))
 
 (defn run-pair [prog]
-  (loop [ss [(assoc init "p" 0 :prog prog)
-             (assoc init "p" 1 :prog prog)]]
-    #_(clojure.pprint/pprint {:ss (map #(dissoc % :prog) ss)})
+  (loop [ss [(assoc init 'p 0 :prog prog)
+             (assoc init 'p 1 :prog prog)]]
     (if (every? #(and (:blocked %) (empty? (:outbox %))) ss)
-      (prn :part-2 (get-in ss [1 :send-count]))
+      (do
+        (prn :part-2 (get-in ss [1 :send-count]))
+        (-> (get ss 0) :trace))
       (recur (->> ss
                   (move-msgs 0 1)
                   (move-msgs 1 0)
@@ -85,5 +88,5 @@
 
 ;; 127 is too low (was running each thread only to the first blocking state)
 
-#_(run-pair [["snd" "1"] ["snd" "2"] ["snd" "p"] ["rcv" "a"] ["rcv" "b"] ["rcv" "c"] ["rcv" "d"]])
-(run-pair input)
+(run-pair '[[snd 1] [snd 2] [snd p] [rcv a] [rcv b] [rcv c] [rcv d]])
+#_(run-pair input)
--- src/chouser/day18b.clj
+++ src/chouser/day18b.clj
@@ -1,22 +1,24 @@
 (ns chouser.day18b
   (:require [clojure.java.io :as io])
-  (:import (clojure.lang ExceptionInfo)))
+  (:import (clojure.lang ExceptionInfo IFn IDeref)))
 
 (def input
   (for [line (line-seq (io/reader "resources/input18.txt"))]
     (read-string (str \[ line \]))))
 
-(defn compile-body [prog ip]
+(def ^:dynamic *trace* nil)
+
+(defn compile-body [prog rip trace?]
   (->>
    prog
-   (mapcat (fn [i [op x y]]
-             (if (= 'jgz op)
-               [`(if (pos? ~x)
-                   (do #_(println (pr-str :jump :p ~'p :i ~i :x ~x :y ~y :to (+ ~i ~y)))
-                       (recur ~(if (number? y) (+ i y) `(+ ~i ~y))))
-                   ~(compile-body (drop (inc i) prog) (inc i)))
-                :done]
-               [(case op
+   (mapcat (fn [ip [op x y :as cmd]]
+             (->
+              (if (= 'jgz op)
+                [`(if (pos? ~x)
+                    (recur ~(if (number? y) (+ ip y) `(+ ~ip ~y)))
+                    (recur ~(inc ip)))
+                 :done]
+                [(case op
                    set `(set! ~x ~y)
                    add `(set! ~x (+ ~x ~y))
                    mul `(set! ~x (* ~x ~y))
@@ -32,12 +34,19 @@
                             (set! ~'cin (rest ~'cin))
                             (if (= :done val#)
                               (throw (ex-info "done" {:done 2}))
-                              (set! ~x (long val#))))))]))
-           (drop ip (range)))
+                              (set! ~x (long val#))))))])
+              ((fn [forms]
+                 (if-not trace?
+                   forms
+                   (update forms 0
+                           (fn [form]
+                             `(do (swap! ~'*trace* conj {:ip ~ip :cmd '~cmd :state (deref ~'this) :recur-at ~rip})
+                                  ~form))))))))
+           (drop rip (range)))
    (take-while #(not= % :done))
    (cons `do)))
 
-(defn compile-fn [prog]
+(defn compile-fn [prog & {:keys [trace?]}]
   (let [regs (map #(with-meta (symbol (str (char %))) {:tag 'long, :unsynchronized-mutable true})
                   (range (int \a) (int \q)))
         Prog (gensym "Prog-")
@@ -47,11 +56,14 @@
                        ~(with-meta 'cout {:unsynchronized-mutable true})
                        ~'blk
                        ~@regs]
-         clojure.lang.IFn
-         (invoke [this]
-           (loop [~'ip (long 0)]
-             (case ~'ip
-               ~@(mapcat (fn [i prog] [i (compile-body prog i)])
+         IDeref
+         (deref [~'this] (into {} (map (fn [k# v#] (when-not (zero? v#) [k# v#]))
+                                       '~regs [~@(map #(with-meta % nil) regs)])))
+         IFn
+         (invoke [~'this]
+           (loop [~'rip (long 0)]
+             (case ~'rip
+               ~@(mapcat (fn [rip prog] [rip (compile-body prog rip trace?)])
                          (range)
                          (take-while seq (iterate rest prog)))))))
        (fn run# [cin# cout# blk# ~pid]
@@ -62,14 +74,18 @@
 (defn run-pair [prog]
   (let [a->b (repeatedly promise)
         b->a (repeatedly promise)
-        f (eval (compile-fn prog))
+        f (eval (compile-fn prog :trace? true))
         blk (atom 0)
-        a (future (f b->a a->b blk 0))
-        b (future (f a->b b->a blk 1))]
+        trace-a (atom [])
+        trace-b (atom [])
+        a (future (binding [*trace* trace-a] (f b->a a->b blk 0)))
+        b (future (binding [*trace* trace-b] (f a->b b->a blk 1)))]
     (prn :a-done (try @a (catch Exception e e)))
+    #_(def ta trace-a)
+    #_(def tb trace-b)
     (prn :b-done (try @b (catch Exception e e)))
-    (prn (count (take-while true? (map realized? (iterate rest b->a)))))
-    (prn (count (take-while true? (map realized? (iterate rest a->b)))))))
+    @trace-a
+    (prn :part-2 (count (take-while true? (map realized? (iterate rest b->a)))))))
 
 #_(binding [clojure.pprint/*print-right-margin* 150](clojure.pprint/pprint (compile-fn input)))
 #_(run-pair '[[snd 1] [snd 2] [snd p] [rcv a] [rcv b] [rcv c] [rcv d]])
